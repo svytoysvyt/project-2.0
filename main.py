95d6b71486b0279bbbac49ffcb3185476e5252b2
master <svytoslav01@yandex.ru>
2022-01-10 23:36:59 +0300

    progress

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ import sys
 import random
 
 import pygame
+
+WIDTH = 1800
+HEIGHT = 1000
 FPS = 15
+SELECTED = False
 LEVEL_PURPLE = 1
 LEVEL_BLUE = 1
-TURN_PLAYER = True
+TURN_PLAYER = 1
+DIAMETR = 90
+BLUE_EXAMS = []
+PIGS_BLUE = [1, 1, 1, 1, 1, 1, 1, 1]
+PIGS_PURPLE = [1, 1, 1, 1, 1, 1, 1, 1]
+IMAGE = ['data/back.jpg', 'data/back1.jpg', 'data/back2.jpg']
 
-def load_image(name, colorkey=None):
+
+def load_image(name):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
     return image
+
 
 class Board:
     def __init__(self, width, height):
@@ -38,7 +48,7 @@ class Board:
 
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
-                if (row+column) % 2 == 0:
+                if (row + column) % 2 == 0:
                     pygame.draw.rect(
                         surface=surface,
                         color=self.color[0],
@@ -74,43 +84,79 @@ class Board:
                     width=1
                 )
 
+class Pig_purple(pygame.sprite.Sprite):
+    PURPLE = load_image("purple.png")
+
+    def __init__(self, number):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(Pig_blue.BLUE, (DIAMETR, DIAMETR))
+        self.rect = self.image.get_rect()
+        self.rect.x = 525 + 100 * number
+        self.rect.y = 85 + 100 * (LEVEL_PURPLE - 1)
+        self.number = number
+        self.vector = (0, 0)
+        self.fast = 0
+        self.life = True
+
+    def update(self, types, randomka, *args):
+        global SELECTED
+        if self.life and types == 0 and (self.rect.x > 1385 or self.rect.x < 525 or self.rect.y < 85 or self.rect.y > 885):
+            self.rect.x = WIDTH + DIAMETR
+            self.rect.y = HEIGHT + DIAMETR
+            PIGS_PURPLE[self.number] = 0
+        elif self.life and types == 1 and self.number == randomka:
+            pass
+
 
 class Pig_blue(pygame.sprite.Sprite):
     BLUE = load_image("blue.png")
 
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self.image = pygame.transform.scale(Pig_blue.BLUE, (90, 90))
+    def __init__(self, number):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(Pig_blue.BLUE, (DIAMETR, DIAMETR))
         self.rect = self.image.get_rect()
-        self.rect.x = 525 + 100 * i
+        self.rect.x = 525 + 100 * number
         self.rect.y = 785 - 100 * (LEVEL_BLUE - 1)
-        self.selected = False
+        self.number = number
+        self.vector = (0, 0)
+        self.fast = 0
+        self.life = True
 
-        def select(self, *args, **kwargs):
-
-
-        def update(self):
-            if self.rect.x > 1385 or self.rect.x < 525 or self.rect.y < 85 or self.rect.y > 885:
-
-
-
-
+    def update(self, types, *args):
+        global SELECTED
+        if self.life and types == 0 and (self.rect.x > 1385 or self.rect.x < 525 or self.rect.y < 85 or self.rect.y > 885):
+            self.rect.x = WIDTH + DIAMETR
+            self.rect.y = HEIGHT + DIAMETR
+            PIGS_BLUE[self.number] = 0
+            self.life = False
+        elif self.life and types == 1 and self.rect.x <= args[0][0] <= self.rect.x + DIAMETR and \
+                args[0][1] <= self.rect.y + DIAMETR <= self.rect.y and SELECTED == False:
+            print(self.number)
+            SELECTED = True
 
 
 PURPLE = load_image("purple.png")
-IMAGE = ['data/back.jpg', 'data/back1.jpg', 'data/back2.jpg']
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.Surface((1800, 950))
-    pygame.display.set_caption('чапаев')
-    all_sprites1 = pygame.sprite.Group()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Чапаев')
+    randomka = random.randint(0, 7)
+    while not PIGS_PURPLE[randomka]:
+        randomka = random.randint(0, 7)
+    blue_pigs = pygame.sprite.Group()
     all_sprites2 = pygame.sprite.Group()
+    blue_pigs.add(Pig_blue(1))
+    blue_pigs.add(Pig_blue(2))
+    blue_pigs.add(Pig_blue(3))
+    blue_pigs.add(Pig_blue(4))
+    blue_pigs.add(Pig_blue(5))
+    blue_pigs.add(Pig_blue(6))
+    blue_pigs.add(Pig_blue(7))
+    blue_pigs.add(Pig_blue(0))
     for i in range(8):
         # можно сразу создавать спрайты с указанием группы
-
-
         purple = pygame.sprite.Sprite()
-        purple.image = pygame.transform.scale(PURPLE, (90, 90))
+        purple.image = pygame.transform.scale(PURPLE, (DIAMETR, DIAMETR))
         purple.rect = purple.image.get_rect()
         # задаём случайное местоположение бомбочке
         purple.rect.x = 525 + 100 * i
@@ -124,12 +170,15 @@ if __name__ == '__main__':
     running = True
     screen.blit(back, back_rect)
     board.render(screen)
-    all_sprites1.draw(screen)
-    all_sprites2.draw(screen)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if TURN_PLAYER == 1:
+                    blue_pigs.update(1, randomka, event.pos)
+        blue_pigs.update(0)
+        blue_pigs.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
